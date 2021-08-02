6a202366f18d95268a18a6dec86cb879e5eb090f
Mikołaj Chajewski <51335952+mikolka9144@users.noreply.github.com>
2021-08-02 10:50:02 +0200

    Added some Class-related functionality

--- Pix_API/APIServerResolver.cs
+++ Pix_API/APIServerResolver.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using System.Linq;
+using System.Reflection;
+using PixBlocks.Server.DataModels.DataModels;
+using Pix_API.Providers;
 
 namespace Pix_API
 {
     internal class APIServerResolver
     {
         private readonly Main_Logic logic;
+        private readonly IUserDatabaseProvider userDatabaseProvider;
         private readonly Type logic_type;
 
-        public APIServerResolver(Main_Logic logic)
+        public APIServerResolver(Main_Logic logic,IUserDatabaseProvider userDatabaseProvider)
         {
             this.logic = logic;
+            this.userDatabaseProvider = userDatabaseProvider;
             logic_type = logic.GetType();
         }
         public string Execute_API_Method(string name,string[] parameters)
@@ -22,13 +28,49 @@ namespace Pix_API
                 Console.WriteLine($"method {name} not implemented");
                 return "";
             }
-            Console.WriteLine($"method {name} was called");
+            Console.WriteLine($"method {name} was issued to be called");
 
-            var object_response = logic_type.GetMethod(name)
-            .Invoke(logic,new[] { parameters });
+            var method = logic_type.GetMethod(name);
+            var deserialized_parameters = ParseParameters(parameters,method.GetParameters());
+
+            if (!CallerhasPermissionToRun(method,deserialized_parameters)) return "";
+
+            Console.WriteLine($"method {name} was called");
+            var object_response = method.Invoke(logic,deserialized_parameters);
 
             var object_json = JsonConvert.SerializeObject(object_response);
             return object_json;
         }
+
+        private bool CallerhasPermissionToRun(MethodInfo method,object[] method_arguments)
+        {
+            var IsReuiredAuthentication = Attribute.IsDefined(method, typeof(RequiresAuthentication));
+            if (IsReuiredAuthentication)
+            {
+                var auth = method_arguments.Last() as AuthorizeData;
+                var server_user = userDatabaseProvider.GetUser(auth.UserId);
+
+                var IsAuthenticationValid = auth.PasswordMD5 == server_user.Md5Password;
+                return IsAuthenticationValid;
+            }
+            return true;
+        }
+
+
+
+        private object[] ParseParameters(string[] parameters,ParameterInfo[] parameters_types)
+        {
+            var output = new List<object>();
+            for (int i = 0; i < parameters_types.Length; i++)
+            {
+                var parameter_type = parameters_types[i].ParameterType;
+                var deserialised_object = 
+                JsonConvert.DeserializeObject(parameters[i],parameter_type);
+                output.Add(deserialised_object);
+            }
+            return output.ToArray();
+        }
     }
+    public class RequiresAuthentication : Attribute { }
+
 }
--- Pix_API/Providers/ContainersProviders/StudentClassesProvider.cs
+++ Pix_API/Providers/ContainersProviders/StudentClassesProvider.cs
@@ -1,22 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using PixBlocks.Server.DataModels.DataModels;
 namespace Pix_API.Providers.ContainersProviders
 {
     public class StudentClassesProvider : Storage_Provider<StudentsClass>, IStudentClassProvider
     {
-        public StudentClassesProvider(DataSaver<List<StudentsClass>> saver) : base(saver)
+        private readonly IStudentClassBinding classBinding;
+
+        public StudentClassesProvider(DataSaver<List<StudentsClass>> saver, IStudentClassBinding classBinding) : base(saver)
         {
+            this.classBinding = classBinding;
+        }
+
+        public void AddClassForUser(StudentsClass studentsClass, int userId)
+        {
+                var AllQuestionResults = GetUserObjectOrCreateNew(userId);
+                studentsClass.Id = AllQuestionResults.Obj.Count;
+                AddObject(studentsClass, userId);
+                classBinding.Add(userId, studentsClass.Id.Value);
+        }
+
+        public void EditClassForUser(StudentsClass studentsClass, int userId)
+        {
+            AddOrUpdateObject(studentsClass, userId, (a, b) => a.Id == b.Id);
         }
 
         public List<StudentsClass> GetClassesForUser(int Id)
         {
             return GetAllObjectsForUser(Id);
         }
-    }
+        public StudentsClass GetStudentsClassForUser(int userId,int classId)
+        {
+            var AllQuestionResults = GetUserObjectOrCreateNew(userId);
+            return AllQuestionResults.Obj.FirstOrDefault(s => s.Id == classId);
+        }
 
+        public List<int> GetStudentsClassForUserInClassOfUser(int userId, int classId)
+        {
+            var AllQuestionResults = GetUserObjectOrCreateNew(userId);
+            return classBinding.Get(userId,classId).students_ids;
+        }
+
+        public void RemoveClassForUser(StudentsClass studentsClass, int userId)
+        {
+            var AllQuestionResults = GetUserObjectOrCreateNew(userId);
+            AllQuestionResults.Obj.RemoveAll((obj) => obj.Id == studentsClass.Id);
+            classBinding.Remove(userId, studentsClass.Id.Value );
+        }
+    }
     public interface IStudentClassProvider
     {
         List<StudentsClass> GetClassesForUser(int Id);
+        StudentsClass GetStudentsClassForUser(int userId, int classId);
+        void AddClassForUser(StudentsClass studentsClass, int userId);
+        void EditClassForUser(StudentsClass studentsClass, int userId);
+        void RemoveClassForUser(StudentsClass studentsClass, int userId);
+        List<int> GetStudentsClassForUserInClassOfUser(int userID,int classID);
     }
 }
