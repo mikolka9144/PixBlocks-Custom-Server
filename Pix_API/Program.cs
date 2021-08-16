7d2bef70075d4754586e6caf60bcd7854c9a94b9
Mikołaj Chajewski <mikolajchajewski@gmail.com>
2021-08-16 23:22:51 +0200

    Save

--- Pix_API/Program.cs
+++ Pix_API/Program.cs
@@ -18,6 +18,7 @@ using PixBlocks.Server.DataModels.DataModels.Championsships;
 using System.Reflection;
 using Pix_API.CoreComponents;
 using Pix_API.Interfaces;
+using Ninject;
 
 namespace Pix_API
 {
@@ -25,37 +26,11 @@ namespace Pix_API
     {
         public static void Main(string[] args)
         {
-            var raw_countries_csv = File.ReadAllText("./countries.csv");
-
-            var users_saver = new DiskDataSaver<User>("./Users/");
-            var question_result_saver = new DiskDataSaver<List<QuestionResult>>("./QuestionResults/");
-            var studentClassesSaver = new DiskDataSaver<List<StudentsClass>>("./Classes/");
-            var question_edits_saver = new DiskDataSaver<List<EditedQuestionCode>>("./QuestionsCodes/");
-            var comments_saver = new DiskDataSaver<List<Comment>>("./Comments/");
-            var student_class_exams_saver = new DiskDataSaver<ServerExam>("./Exams/");
-            var toyShop_saver = new DiskDataSaver<ToyShopData>("./ToyShops/");
-            var schools_saver = new DiskDataSaver<School>("./Schools/");
-            var championshipsMetadata_saver = new DiskDataSaver<Championship>("./Championships/");
-
-            var usersProvider = new UserDatabaseProvider(users_saver);
-            var questionResultProvider = new QuestionResultProvider(question_result_saver);
-            var editQuestionProvider = new QuestionEditsProvider(question_edits_saver);
-            var toyShopProvider = new ToyShopProvider(toyShop_saver);
-            var studentClassesProvider = new StudentClassesProvider(studentClassesSaver,usersProvider);
-            var studentClassExamsProvider = new StudentClassExamsProvider(student_class_exams_saver);
-            var userCommentsProvider = new UserCommentsProvider(comments_saver);
-            var SchoolsProvider = new SchoolsProvider(schools_saver);
-            var countriesProvider = new CountriesProvider();
-            var staticNotyficationProvider = new StaticNotyficationProvider();//TODO
-            var staticChampionshipsProvider = new ChampionshipProvider(championshipsMetadata_saver);
-            var brandingProvider = new BrandingProvider();
+            var kerner = new StandardKernel();
+            kerner.Load("./Pix_API.Providers.dll");
 
             var server = new ConnectionRecever();
-            var resolver = new APIServerResolver(new Main_Logic(countriesProvider,
-                usersProvider, questionResultProvider, editQuestionProvider,
-                toyShopProvider,staticNotyficationProvider,
-                staticChampionshipsProvider,studentClassesProvider,
-                studentClassExamsProvider,userCommentsProvider,SchoolsProvider,brandingProvider),usersProvider);
+            var resolver = kerner.Get<APIServerResolver>();
 
             server.OnCommandReceved += (a, b, c) => OnCommand(a, b, c, resolver);
             server.Start_Lisening("http://*:8080/");
