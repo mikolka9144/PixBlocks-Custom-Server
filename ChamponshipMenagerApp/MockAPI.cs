424eca46931ebc16e16ce2217e768b8899bb74ff
Mikołaj Chajewski <mikolajchajewski@gmail.com>
2021-09-24 22:59:28 +0200

    Integrated Championship helper app

---
+++ ChamponshipMenagerApp/MockAPI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using PixBlocks.Server.DataModels.DataModels;
+using PixBlocks.Server.DataModels.DataModels.Championsships;
+
+namespace ChamponshipMenagerApp
+{
+    public class MockAPI:IChampionshipsAPI
+    {
+        private List<Championship> championships;
+
+        public MockAPI()
+        {
+            championships = new List<Championship>();
+        }
+
+        public int AddChampionship(Championship championship)
+        {
+            championship.Id = championships.Count;
+            championships.Add(championship);
+            return championship.Id;
+        }
+
+        public List<Championship> GetAllActiveChampionships()
+        {
+            return championships;
+        }
+
+        public List<Countrie> GetAllCountries() => new List<Countrie>()
+        {
+            new Countrie(0,"test","PL"),
+            new Countrie(1,"testwww","PL"),
+            new Countrie(2,"testcxxxx","PL")
+        };
+        public void RemoveChampionships(int ChampionshipId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateChampionship(Championship championship)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChamponshipMenagerApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xwt;
+namespace ChamponshipMenagerApp
+{
+    class MainClass
+    {
+        private static LoginWindow mainWindow;
+
+        static void Main()
+        {
+            InitaliseLibraly();
+
+            mainWindow = new LoginWindow();
+
+            mainWindow.Show();
+            Application.Run();
+            mainWindow.Dispose();
+        }
+
+        private static void InitaliseLibraly()
+        {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Unix:
+                    Application.Initialize(ToolkitType.Gtk);
+                    break;
+                case PlatformID.Win32NT:
+                    Application.Initialize(ToolkitType.Wpf);
+                    break;
+            }
+        }
+
+
+
+    }
+}
